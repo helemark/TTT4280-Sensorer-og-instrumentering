9917b4d30ef550ace4a32c4ab10787ed4b4ff3d5
Helene Markeng <51700394+helemark@users.noreply.github.com>
2021-02-26 13:50:36 +0100

    Update prossesering_radar.py

--- Lab 3/prossesering_radar.py
+++ Lab 3/prossesering_radar.py
@@ -4,12 +4,14 @@ import scipy.signal as signal
 
 
 
+
+
 '''Konstanter'''
 
 f0 = 2.413e10    #denne tregner vi vel forsåvidt ikke
 fs = 31250          #samplingsfrekvensen på Pi'en
 c = 3e8
-k=200
+k=100
 
 def raspi_import(path, channels=5):
     """
@@ -26,14 +28,13 @@ def raspi_import(path, channels=5):
 
 
 # Import data from bin file
-sample_period, data = raspi_import('fredag_koffert10.bin')
+sample_period, data = raspi_import('fredag_bil3_2ms.bin')
 
 #data = signal.detrend(data, axis=0)  # removes DC component for each channel
 sample_period *= 1e-6  # change unit to micro seconds
 
 # Generate time axis
 num_of_samples = data.shape[0]  # returns shape of matrix
-t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 
 
 '''Helene's Funksjoner'''
@@ -42,26 +43,24 @@ t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 def fourier(vector):
     sp = np.fft.fft(vector)
     freq = np.fft.fftfreq(vector.shape[-1])
-    return sp, abs(freq)
+    return sp, freq
 
 
 def speed(sp, freq):
-    for i in range(k):
-        sp = np.delete(sp, 0)
-    for i in range(k):
-        sp = np.delete(sp, len(sp)-1)
-        
-    corr = signal.correlate(data_0, data_1)
-    phase = np.argmax(corr)-fs
-
-    plt.plot(sp)
-    plt.show()
-    index = int(np.argmax(abs(sp.real)))+k
-    fd = freq[index]*fs
+    index = np.argmax(abs(sp))
+    fd = abs(freq[index]*fs)
+    print('fd:', fd)
     speed = (fd*c)/(2*f0)
-    if phase < 0:
-        speed = -speed
     return speed
+
+def butter_bandstop_filter(data, lowcut, highcut, fs, order):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+
+    i, u = signal.butter(order, [low, high], btype='bandstop')
+    y = signal.lfilter(i, u, data)
+    return y
     
 
 
@@ -82,8 +81,8 @@ def speed_of_time(vector, k, fs):
 
 '''HER BEGYNNER SELVE KODEN'''
 
-data_0 = data[:,0]-np.mean(data[:,0])
-data_1 = data[:,1]-np.mean(data[:,1])
+data_0 = data[:,0][k*3:]-np.mean(data[:,0])
+data_1 = data[:,1][k*3:]-np.mean(data[:,1])
 
 
 plt.plot(data_1)
@@ -94,27 +93,23 @@ plt.show()
 
 """FILTRERING"""
 
-#soshp = signal.butter(10, 80, 'hp', fs = fs, output='sos')
+soshp = signal.butter(10, 80, 'hp', fs = fs, output='sos')
 soslp = signal.butter(10, 800, 'lp', fs = fs, output='sos')
 
-#data_0 = signal.sosfilt(soshp, data_0)
+data_0 = signal.sosfilt(soshp, data_0)
 data_0 = signal.sosfilt(soslp, data_0)
+data_0 = butter_bandstop_filter(data_0, 650, 690, fs, 4)
 
-
-#data_1 = signal.sosfilt(soshp, data_1)
+data_1 = signal.sosfilt(soshp, data_1)
 data_1 = signal.sosfilt(soslp, data_1)
+data_1 = butter_bandstop_filter(data_1, 650, 690, fs, 4)
+
+
 
 plt.plot(data_1)
 plt.plot(data_0)
 plt.title('Etter filtrering')
 plt.show()
-
-
-
-
-#window = np.hanning(fs*10)
-#sp, freq = fourier(data_0*window)
-
 
 sp0, freq0 = fourier(data_0)
 sp1, freq1 = fourier(data_1)
@@ -126,10 +121,13 @@ plt.title('Spectre')
 plt.show()
 '''
 
-sp, freq = fourier(1j*data_0+data_1)
-plt.plot(freq, abs(sp))
+sp, freq = fourier(1j*data_0 + data_1)
+plt.plot(freq, abs(sp.real),'r')
 plt.title('IF_I+jIF_Q')
+sp, freq = fourier(data_1)
+plt.plot(freq, abs(sp.real),'b')
 plt.show()
+
 
 '''
 plt.plot(sp)
